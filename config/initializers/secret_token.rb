d89fec2d1cef28958e7990bea4eeecf53ef4cae6
Laurent <lbuffevant@gmail.com>
2011-09-02 10:32:38 +0200

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fuzion::Application.config.secret_token = '5b60c6d7bfb9ca926f1a5527868de681aa795d203044ccd2213cbd00c9b89b9e7f94f80966ffafbd79930018e5b7a9f9ba5a2559fa05a775e4c8e51b6d1eda5d'
