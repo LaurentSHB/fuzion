42562d28b4da5b67a488c34f1c59d5b5c4517c03
Laurent <lbuffevant@gmail.com>
2011-09-21 17:35:49 +0200

    style backoffice & tables & connexion + continue notation system

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,11 @@ class Ability
     can :view_super_admin_side do
       user.is_super_admin? && user.active
     end
+
+    can :set_notation, Match do |match|
+      p = user.participations.find_by_match_id(match.id)
+      !p.blank? && p.convocation && !p.notation_done
+    end
         
 #
 #    can :view_details, Program do |program|
