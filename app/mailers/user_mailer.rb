cad8e8eaf032e55b16e4d84ec178dd10c6da38d4
Laurent <lbuffevant@gmail.com>
2011-11-23 12:04:44 +0100

    remove stef to the user mailing list

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default :from => SYSTEM_MAILER
 
   def new_comment(comment)
-    to = User.activated.collect{|u| u.email} - [comment.user.email]
+    to = User.activated.collect{|u| u.email} - [comment.user.email, "smagnin.osteo@gmail.com"]
     @comment = comment
     mail(:to => to, :subject => "PMI2 - Nouveau commentaire sur le site")
   end
