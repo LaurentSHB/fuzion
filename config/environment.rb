71e67f917c74c6bdf43d4e7837b79b6e60caec87
Laurent <lbuffevant@gmail.com>
2013-08-30 13:39:14 +0200

    MAJ saison et palmarès

--- config/environment.rb
+++ config/environment.rb
@@ -7,5 +7,5 @@ Fuzion::Application.initialize!
 
 SYSTEM_MAILER = "Parlez Moi d'Immo 2 <no-reply@footfuzion.fr>"
 
-YEARS = [["Saison 2012-2013", 2012], ["Saison 2011-2012", 2011], ["Saison 2010-2011", 2010], ["Saison 2009-2010", 2009], ["Saison 2008-2009", 2008]]
-CURRENT_YEAR = 2012
+YEARS = [["Saison 2013-2014", 2013], ["Saison 2012-2013", 2012], ["Saison 2011-2012", 2011], ["Saison 2010-2011", 2010], ["Saison 2009-2010", 2009], ["Saison 2008-2009", 2008]]
+CURRENT_YEAR = 2013
